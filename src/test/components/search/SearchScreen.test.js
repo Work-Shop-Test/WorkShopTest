1a358ced67f87d263372454f2468dae91396cb48
Luis Chávez <luischdu@gmail.com>
2021-06-11 12:32:37 -0500

    tests

---
+++ src/test/components/search/SearchScreen.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { SearchScreen } from '../../../components/search/SearchScreen';
+
+
+describe('Pruebas en SearchScreen', () => {
+
+    test('Mostrar valor inicial', () => {
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search']}>
+                <Route path="/search" component={ SearchScreen } />
+            </MemoryRouter>
+        );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('.alert-info').text().trim() ).toBe('Search a hero');
+ 
+    })
+})
